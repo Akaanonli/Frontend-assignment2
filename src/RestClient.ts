3413f87605442cd7a2daf484b01556cca89c2152
Raul Cepeda-Vestly <raul.cepeda-vestly@dnb.no>
2021-04-10 12:45:59 +0200

    kommentert ut en del kode relatert til forsøk på login og token

--- src/RestClient.ts
+++ src/RestClient.ts
@@ -12,12 +12,21 @@ export class RestClient {
         const url = `${RestClient.baseUrl}/configurations`
         const response = await window.fetch(url)
         return await response.json()
-    }
-    /*
+    }    
+    /*       
     static async getEnvironment(id: number) : Promise<any> {
         const url = `${RestClient.baseUrl}/environments/${id}`
         const response = await fetch(url)
         return await response.json()
-    }*/
+    }
+    
+    -- Raul prøver seg på en funksjon for henting av admin credentials --
+        static async getCredentials() : Promise<any> {
+        const url = `${RestClient.baseUrl}/administrators`
+        const response = await fetch(url)
+        return await response.json()
+    }
+
+    */
 
 }
