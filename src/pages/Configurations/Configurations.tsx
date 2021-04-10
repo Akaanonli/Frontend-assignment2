a7ba20704392cea91a8ac39c26b30c12168e13c7
Alina Dobrica <alina.dobrica@dnb.no>
2021-04-11 01:14:18 +0200

    one small change

--- src/pages/Configurations/Configurations.tsx
+++ src/pages/Configurations/Configurations.tsx
@@ -4,7 +4,7 @@ import { RestClient } from "../../RestClient"
 import { Button } from '@dnb/eufemia/components';
 import { Heading } from '@dnb/eufemia/components';
 
-export default function Configurations() {
+function Configurations() {
 
 	let[configurations, setConfigurations]= React.useState<Array<any>>([])
 
@@ -31,6 +31,78 @@ export default function Configurations() {
 		</div>
 	)
 }
+
+
+export default function useAddConfigurationFormMarkup() {
+
+	const [value, setValue] = React.useState(0) 
+
+	const handleSubmit = (e: any) => {
+		e.preventDefault();
+		let configuration = {
+			id: (document.getElementById('id') as HTMLInputElement).value,
+			appName:  (document.getElementById('appName') as HTMLInputElement).value,
+			environment: (document.getElementById('environment') as HTMLInputElement).value,
+			key: (document.getElementById('environment') as HTMLInputElement).value,
+			value: (document.getElementById('environment') as HTMLInputElement).value,
+			lastModifiedUserId: (document.getElementById('environment') as HTMLInputElement).value,
+			lastModifiedDttm: (document.getElementById('environment') as HTMLInputElement).value
+		}
+		RestClient.addConfiguration(configuration)
+		          .then( () => {
+					  window.alert('Thanks!')
+					  e.target.reset()
+					  Configurations().push(configuration)
+					  setValue(value => value + 1)     // Dummy state change, to trigger re-render
+				  })
+				  .catch(err => alert(err))
+	}
+
+	return (
+		<div>
+			<h2>Add Configuration</h2>
+			<form onSubmit={handleSubmit}>
+				<p>
+					<label htmlFor='id'>Id</label>
+					<input id='id' type='text'/>
+				</p>
+				<p>
+					<label htmlFor='appName'>Application Name</label>
+					<input id='appName' type='text'/>
+				</p>
+				<p>
+					<label htmlFor='environment'>Environment</label>
+					<input id='environment' type='text'/>
+				</p>
+
+				<p>
+					<label htmlFor='key'>Key</label>
+					<input id='key' type='text'/>
+				</p>
+				<p>
+					<label htmlFor='value'>Value</label>
+					<input id='value' type='text'/>
+				</p>
+				<p>
+					<label htmlFor='lastModifiedDttm'>Last Modified at:</label>
+					<input id='lastModifiedDttm' type='text'/>
+				</p>
+
+
+				<p>
+					<label htmlFor='lastModifiedUserId'>Last Modified by user:</label>
+					<input id='lastModifiedUserId' type='text'/>
+				</p>
+
+				<p>
+					<label>&nbsp;</label> {/* Placeholder */}
+					<button>Add configuration</button>
+				</p>
+			</form>
+		</div>
+	)
+}
+
 
 
 
