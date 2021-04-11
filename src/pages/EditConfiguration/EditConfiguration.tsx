7e78e53914dbe54f368ac072a756e46ab3dba378
Alina Dobrica <alina.dobrica@dnb.no>
2021-04-11 14:05:44 +0200

    commit edit config

---
+++ src/pages/EditConfiguration/EditConfiguration.tsx
@@ -0,0 +1,139 @@
+import React , {useState, useEffect} from "react";
+import { useParams } from "react-router-dom";
+import { RestClient } from "../../RestClient";
+import { Button } from '@dnb/eufemia/components';
+import { Heading } from '@dnb/eufemia/components';
+import ReactDOM from 'react-dom';
+
+
+export default function EditConfiguration() {
+
+	let {id}: any =useParams()
+
+	let[environment, setEnvironment] = React.useState<any>(undefined)
+
+	React.useEffect(()=> {
+		RestClient.getEnvironment(id)
+					.then(environment => setEnvironment(environment))
+					.catch(err => alert(err))
+	},[id])
+
+	if (environment) {
+		return (
+			<React.Fragment>
+				<EnvDetails {...environment}/>
+				<EnvConfigurations {...environment}/>
+			</React.Fragment>
+		)
+	} else {
+		return <p>Please wait...</p>
+	}
+}
+
+function EnvDetails (environment: any){
+	return(
+		<div>
+			<p>
+				<label> Key name: </label>
+				<span>{environment.keyName}</span>
+			</p>
+			<p>
+				<label> Long Name: </label>
+				<span>{environment.longName}</span>
+			</p>
+		</div>
+	)
+}
+
+
+function EnvConfigurations(environment: any) {
+	return (
+		<React.Fragment>
+			{editConfigurationsMarkup(environment)}
+			{editConfigurationFormMarkup(environment)}
+		</React.Fragment>
+	)
+}
+
+function editConfigurationsMarkup(environment: any) {
+	if (!environment.configurations || !environment.configurations.length) {
+		return <div>No configurations yet</div>
+	}
+	else {
+		return (
+			<div>
+				<h2>Configurations</h2>
+				{environment.configurations.map((r:any,i: number) => 
+					<p key={i}>
+						<span className='appName'>{r.appName}</span>
+						<span className='key'>{r.key}</span>
+						<span className='value'> {r.value}</span>
+						<span className='lastModifiedUserId'> {r.lastModifiedUserId}</span>
+						<span className='lastModifiedDttm'> [{r.lastModifiedDttm}]</span>
+					</p>
+				)}
+			</div>
+		)
+	}
+}
+
+
+
+function editConfigurationFormMarkup(environment: any) {
+
+	// Dummy state, which we will update after the user has added a review.
+	// By updating this state, we are telling React to re-render the component (i.e. to show the new review).
+	const [value, setValue] = React.useState(0) 
+
+	const handleSubmit = (e: any) => {
+		e.preventDefault();
+		let configuration = {
+			appName:  (document.getElementById('appName') as HTMLInputElement).value,
+			key: (document.getElementById('key') as HTMLInputElement).value,
+			value: (document.getElementById('value') as HTMLInputElement).value,
+			lastModifiedUserId: (document.getElementById('lastModifiedUserId') as HTMLInputElement).value,
+			lastModifiedDttm: (document.getElementById('lastModifiedDttm') as HTMLInputElement).value
+		}
+		RestClient.changeConfiguration(environment.id, configuration)
+		          .then( () => {
+					  window.alert('Thanks dude!')
+					  e.target.reset()
+					  environment.configurations.push(configuration)
+					  setValue(value => value + 1)     // Dummy state change, to trigger re-render
+				  })
+				  .catch(err => alert(err))
+	}
+
+	return (
+		<div>
+			<h2>Edit Configuration details</h2>
+			<form onSubmit={handleSubmit}>
+				<p>
+					<label htmlFor='appName'>Application Name</label>
+					<input id='appName' type='text'/>
+				</p>
+				<p>
+					<label htmlFor='key'>Key</label>
+					<input id='key' type='text'/>
+				</p>
+				<p>
+					<label htmlFor='value'>Value</label>
+					<input id='value' type='text'/>
+				</p>
+				<p>
+					<label htmlFor='lastModifiedDttm'>Last Modified at:</label>
+					<input id='lastModifiedDttm' type='text'/>
+				</p>
+				<p>
+					<label htmlFor='lastModifiedUserId'>Last Modified by user:</label>
+					<input id='lastModifiedUserId' type='text'/>
+				</p>
+
+				<p>
+					<label>&nbsp;</label> {}
+					<button>Submit changes</button>
+				</p>
+			</form>
+		</div>
+	)
+}
---
+++ src/pages/EditConfigurations/EditConfigurations.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { RestClient } from "../../RestClient"
+import { Button } from '@dnb/eufemia/components';
+import { Heading } from '@dnb/eufemia/components';
+
+
+export default function EditConfigurations() {
+
+	let[environments, setEnvironments]= React.useState<Array<any>>([])
+
+	React.useEffect(()=>{
+		RestClient.getEnvironments()
+					.then(environments => setEnvironments(environments))
+
+	}, [])
+
+	return(
+		<div> 
+		<h1>Environments</h1>
+		{environments.map((c:any, i:number) =>
+			<Link key={i} className='blockLink' to={`editconfiguration/${c.id}`}>
+			/<Button
+					variant="secondary"
+					text={c.keyName}
+					icon="chevron_right_medium"
+					size="large"
+				/>
+			</Link>
+			)}
+		</div>
+	)
+}
+
+/*
+export default function ListEnvironments(){
+	return (
+		<React.Fragment>
+     	 	<Heading size="medium">click to show environments</Heading>
+      		<Button onClick={() => secondTry()}>Environments</Button>
+    	</React.Fragment>
+	);
+
+
+	  function secondTry() {
+		const promise = RestClient.getEnvironments()
+		promise.then(data => {
+			var correspondingConfigsForEnvironmentOne = data[0].configurations
+            for(var c of correspondingConfigsForEnvironmentOne) {
+                console.log(c.keyName)
+            }
+		})
+	  
+    }
+}
+*/
