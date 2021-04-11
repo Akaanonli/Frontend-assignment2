1cb04d98ee110c533a82e07119a70acfa67f70cf
Anne Karen Aanonli <Anne.Karen.Aanonli@dnb.no>
2021-04-11 18:39:28 +0200

    Add environment

--- src/pages/Admin/EditEnvironments.tsx
+++ src/pages/Admin/EditEnvironments.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {RestClient } from "./../../RestClient"
+import { useParams } from "react-router-dom";
 import { Heading, Button } from '@dnb/eufemia/components';
 import { Div, H2 } from '@dnb/eufemia/elements';
 import { Link } from "react-router-dom";
@@ -18,61 +19,82 @@ export default function EditEnvironments(){
     if(environments){
         return (
 
-            /*
-            <React.Fragment>
-                <EnvironmentsInDataBase {...environments} />
+            <React.Fragment>   
+                {EnvironmentsInDataBase(environments)}
+                <AddEnvironmentForm {...environments} />
             </React.Fragment>
-            */
-
-            
-            <Div>
-            <Heading  size= "small">
-                Here i planned the following functions:
-            </Heading>
-           
-            <ul>
-                <li>list of current environments</li>
-                <li>Add environments</li>
-                <li>link to edit environents</li>
-            </ul>
-            </Div>
-            
+  
         )
     } else {
         return <p>Oooh no</p>
     }
 }
 
+function AddEnvironmentForm (environments:any){
+    return(
+        <React.Fragment>
+            {useAddEnvironmentFormMarkup(environments)}
+        </React.Fragment>
+    )
+}
+
 function EnvironmentsInDataBase (environments:any){
     
         return(
             <React.Fragment>
-                <h1>Environments</h1>
-                    
+                <h1>Environments in Database</h1>
+                    <ul>
                     {environments.map((d:any, i: number)=>
-                    <span key={i}>{d.keyName}</span>)}
-                    
+                    <li key={i}>{d.keyName}</li>)}
+                    </ul>
             </React.Fragment>
-		    
-		
-		
-                        
+               
         )
-
 }
-    
 
-/*
-function addEnvironmentForm (environment:any){
 
-    const [value, setValue] = React.useState(0)
 
-    const handleSubmit = (e:any) 0> {
-        e.preventDefault();
-        let environmentToUpdate = {
-            longName: (document.getElementById('longName') as HTMLInputElement).value
-        }
-    }
 
+function useAddEnvironmentFormMarkup(environments: any) {
+
+	const [value, setValue] = React.useState(0) 
+
+	const handleSubmit = (e: any) => {
+		e.preventDefault();
+		let newEnvironment = {
+			keyName:  (document.getElementById('keyName') as HTMLInputElement).value,
+			longName: (document.getElementById('longName') as HTMLInputElement).value
+			
+		}
+		RestClient.addEnvironment(newEnvironment)
+		          .then( () => {
+					  window.alert('Environment added')
+					  e.target.reset()
+					  environments.environment.push(newEnvironment)
+					  setValue(value => value + 1)
+                  })
+				  .catch(err => alert(err))
+	
+                  }
+	return (
+		<div>
+			<h2>Add Environment</h2>
+			<form onSubmit={handleSubmit}>
+				<p>
+					<label htmlFor='keyName'>Short Name</label>
+					<input id='keyName' type='text'/>
+				</p>
+				<p>
+					<label htmlFor='longName'>Long Name</label>
+					<input id='longName' type='text'/>
+				</p>
+				
+
+				<p>
+					<label>&nbsp;</label> {}
+					<button>Add Environment</button>
+				</p>
+			</form>
+		</div>
+	)
 }
-*/
