3ea3741070c8870d1677dbf6121f2041ceaddbca
Raul Cepeda-Vestly <raul.cepeda-vestly@dnb.no>
2021-04-11 22:44:31 +0200

    update buttons on editconfigurations.tsx

--- src/pages/EditConfigurations/EditConfigurations.tsx
+++ src/pages/EditConfigurations/EditConfigurations.tsx
@@ -21,9 +21,8 @@ export default function EditConfigurations() {
 		<h1>Configurations</h1>
 		{configurations.map((c:any, i:number) =>
 			
-			<Link key={i} className='blockLink' to={`editconfiguration/${c.id}`}>
+			<Link key={i} className='blockLink' to={`editconfiguration/${c.id}`} style={{ textDecoration: 'none' }}>
 			<Button
-					variant="secondary"
 					text={c.appName}
 					icon="chevron_right_medium"
 					size="large"
